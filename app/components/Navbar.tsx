292ce50c82b4d650e5fd5738eeb1e739f8b8bb62
saira512dev <512saira@gmail.com>
2025-06-04 20:29:10 +0530

    Start the project and start with next auth

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <img src="" alt="" />
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
